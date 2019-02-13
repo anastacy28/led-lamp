0194e97549e6915206d305a754a58ab7718b8c61
DESKTOP-H9IG1C6\Nastya <anastacy2882@gmail.com>
2019-02-13 10:05:43 +0200

    first commit

---
+++ js/popup.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+$(document).ready(function() {
+	$("#form").submit(function() {
+		$.ajax({
+			type: "POST",
+			url: "../php/mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.js-overlay-thank-you').fadeIn();
+			$(this).find('input').val('');
+			$("#form").trigger("reset");
+		});
+		return false;
+	});
+});
+
+$(document).mouseup(function (e) { 
+	var popup = $('.popup');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+		$('.js-overlay-thank-you').fadeOut();
+	}
+});
---
+++ js/slick.js
@@ -0,0 +1,16 @@
+
+	$('.slick').slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  dots: true,
+});
+
+
+
+
+
+	
